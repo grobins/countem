a56447ce027e90c9cd0a864e7cec48a97327d8a3
Gareth Robins <gareth.robins@gmail.com>
2016-06-05 20:45:34 +1200

    chaged icon to frogs

--- js/countem.js
+++ js/countem.js
@@ -3,19 +3,20 @@ var gameHeight = 600;
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
 
 function preload() {
-    game.load.image('sprite', 'img/circle.png');
+    game.load.image('sprite', 'img/frog_sm.png');
 }
 
 function create() {
 
+    for (var i=0 ; i<7 ; i++ ){
     var sprite = game.add.sprite(-50  , Math.random() * gameHeight    , 'sprite');
 
-    var demoTween = game.add.tween(sprite).to({x: gameWidth + 50, y: Math.random() * gameHeight},2000);
-    // repeats on completion
-    // demoTween.onComplete.add(function(){   
-    //    sprite.x = 0; sprite.y = 0;    
-    //    demoTween.start();    });  
-    demoTween.start();
+    var demoTween = game.add.tween(sprite).to({x: gameWidth + 50, y: Math.random() * gameHeight}, 2000);
+
+       
+        demoTween.start();
+    }
+
 
 
 
