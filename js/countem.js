5015d049fb99acad02e2af4c1133203e14872815
Gareth Robins <gareth.robins@gmail.com>
2016-06-05 21:16:52 +1200

    frogs alternate between left and top edge for starting

--- js/countem.js
+++ js/countem.js
@@ -9,15 +9,25 @@ function preload() {
 function create() {
 
     for (var i=0 ; i<7 ; i++ ){
-    var sprite = game.add.sprite(-50  , Math.random() * gameHeight    , 'sprite');
 
-    var demoTween = game.add.tween(sprite).to({x: gameWidth + 50, y: Math.random() * gameHeight}, 2000);
+        if ( i % 2 == 0){ 
+            // vary starting position along left edge, tweening to right edge
+            setTimeout(function () {
+                var sprite = game.add.sprite(-50, Math.random() * gameHeight, 'sprite');
+                var demoTween = game.add.tween(sprite).to({x: gameWidth + 50, y: Math.random() * gameHeight}, 2000);
+                demoTween.start();
+            }, Math.floor(Math.random() * 1000));
 
-       
-        demoTween.start();
+        } else {
+            // vary starting position along top edge, tweening to bottom edge
+
+            setTimeout(function () {
+                var sprite = game.add.sprite(Math.random() * gameWidth, -50, 'sprite');
+                var demoTween = game.add.tween(sprite).to({x: Math.random() * gameWidth, y: gameHeight + 50}, 2000);
+                demoTween.start();
+            }, Math.floor(Math.random() * 1000));
+
+        }
     }
-
-
-
 
 }
