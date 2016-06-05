d0e18c9608587739acaa4ca7e56f2c47e7cca415
Gareth Robins <gareth.robins@gmail.com>
2016-06-05 21:28:56 +1200

    added random number of frogs

--- js/countem.js
+++ js/countem.js
@@ -1,6 +1,12 @@
 var gameWidth = 800;
 var gameHeight = 600;
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var levels = [1, 2, 3, 5, 7, 12, 19, 31, 50];
+var curLevel = 0;
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function preload() {
     game.load.image('sprite', 'img/frog_sm.png');
@@ -8,7 +14,9 @@ function preload() {
 
 function create() {
 
-    for (var i=0 ; i<7 ; i++ ){
+    var numSprites = Math.max(1, getRandomInt(levels[curLevel] - 2, levels[curLevel] + 2));
+    
+    for (var i=0 ; i<numSprites ; i++ ){
 
         if ( i % 2 == 0){ 
             // vary starting position along left edge, tweening to right edge
@@ -20,7 +28,6 @@ function create() {
 
         } else {
             // vary starting position along top edge, tweening to bottom edge
-
             setTimeout(function () {
                 var sprite = game.add.sprite(Math.random() * gameWidth, -50, 'sprite');
                 var demoTween = game.add.tween(sprite).to({x: Math.random() * gameWidth, y: gameHeight + 50}, 2000);
